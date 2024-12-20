516e7fa3df864c97e2cc8d44cf4e7993d0deb52b
sbx_user1051 <sbx_user1051@169.254.7.10>
2024-12-20 15:11:31 +0000

    Fri Dec 20 15:11:31 UTC 2024

--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -28,10 +28,17 @@ class TestCheckoutSuite():
         assert actual_checkout_value_with_offer == expected_checkout_value_with_offer
 
         #test behaviour when special offers apply
-        test_sku_string_with_offer_2: str = 'ABBACD'
-        actual_checkout_value_with_offer_2 = checkout_solution.checkout(test_sku_string_with_offer_2)
+        test_sku_string_with_offer_b: str = 'ABBACD'
+        actual_checkout_value_with_offer_2 = checkout_solution.checkout(test_sku_string_with_offer_b)
         expected_checkout_value_with_offer_2 = 50 + 50 + 45 + 20 + 15
         assert actual_checkout_value_with_offer_2 == expected_checkout_value_with_offer_2
+
+    def test_checkout_with_multiple_offers(self) -> None:
+        '''
+        test checkout where multiple offers are applicable
+        '''
+
+        test_sku_string_with_multle_special_orders_a = 'AAABAAAAACAAACAA'
 
 
     def test_checkout_illegal_arguments(self) -> None:
@@ -56,6 +63,7 @@ class TestCheckoutSuite():
         #test empty string
         actual_checkout_value_with_empty_string = checkout_solution.checkout('')
         assert actual_checkout_value_with_empty_string == 0
+
 
 
 
