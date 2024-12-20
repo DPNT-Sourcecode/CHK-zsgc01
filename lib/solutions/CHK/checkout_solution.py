ca795a273bcde22856932d436d23ca0d112413c1
sbx_user1051 <sbx_user1051@169.254.7.10>
2024-12-20 14:57:25 +0000

    Fri Dec 20 14:57:25 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -28,17 +28,26 @@ def checkout(skus: str) -> int:
         if sku_char not in sku_count_map: return -1
         sku_count_map[sku_char] += 1
 
-    total_checkout_value = 0
+    sku_price_map: dict[str : int] = {
+        'A': 50,
+        'B': 30,
+        'C': 20,
+        'D': 15
+    }
 
     number_of_item_a_at_normal_price = sku_count_map['A'] % 3
-    number_of_discounted_item_a = sku_count_map['A'] / 3
-    total_price_a = (number_of_item_a_at_normal_price * 50) + (number_of_discounted_item_a * 130)
+    number_of_discounted_item_a = sku_count_map['A'] // 3
+    total_price_a = (number_of_item_a_at_normal_price * sku_price_map['A']) + (number_of_discounted_item_a * 130)
 
     number_of_item_b_at_normal_price = sku_count_map['B'] % 3
-    number_of_discounted_item_b = sku_count_map['B'] / 3
-    total_price_b = (number_of_item_b_at_normal_price * 30) + (number_of_discounted_item_b * 45)
+    number_of_discounted_item_b = sku_count_map['B'] // 3
+    total_price_b = (number_of_item_b_at_normal_price * sku_price_map) + (number_of_discounted_item_b * 45)
 
-    total_price_c = 
+    total_price_c = sku_count_map['C'] * 20
+    total_price_d = sku_count_map['D'] * 15
+
+    return total_price_a + total_price_b + total_price_c + total_price_d
+
 
 
 
