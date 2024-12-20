f3b3263562f4d8da844de476e34d9eb3238ca899
sbx_user1051 <sbx_user1051@169.254.7.10>
2024-12-20 15:08:31 +0000

    Fri Dec 20 15:08:31 UTC 2024

--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -6,19 +6,24 @@ class TestCheckoutSuite():
     suite for testing checkout solution
     '''
 
-    def test_checkout(self) -> None:
+    def test_checkout_without_offers(self) -> None:
         '''
         test the checkout method
         '''
         #test normal behaviour without any applicable special offers
-        test_sku_string_no_offer: str = 'ABCD'
+        test_sku_string_no_offer: str = 'ABCDE'
         actual_checkout_value_no_offer = checkout_solution.checkout(test_sku_string_no_offer)
-        expected_checkout_value_no_offer = 50 + 30 + 20 + 15
+        expected_checkout_value_no_offer = 50 + 30 + 20 + 15 + 40
         assert actual_checkout_value_no_offer == expected_checkout_value_no_offer
 
+
+    def test_checkout_with_single_offers(self) -> None:
+        '''
+        test checkout where single offers are applicable
+        '''
         #test behaviour when special offers apply
-        test_sku_string_with_offer: str = 'ABAACD'
-        actual_checkout_value_with_offer = checkout_solution.checkout(test_sku_string_with_offer)
+        test_sku_string_with_offer_a: str = 'ABAACD'
+        actual_checkout_value_with_offer = checkout_solution.checkout(test_sku_string_with_offer_a)
         expected_checkout_value_with_offer = 130 + 30 + 20 + 15
         assert actual_checkout_value_with_offer == expected_checkout_value_with_offer
 
@@ -28,10 +33,6 @@ class TestCheckoutSuite():
         expected_checkout_value_with_offer_2 = 50 + 50 + 45 + 20 + 15
         assert actual_checkout_value_with_offer_2 == expected_checkout_value_with_offer_2
 
-    
-        #test empty string
-        actual_checkout_value_with_empty_string = checkout_solution.checkout('')
-        assert actual_checkout_value_with_empty_string == 0
 
     def test_checkout_illegal_arguments(self) -> None:
         '''
@@ -48,10 +49,14 @@ class TestCheckoutSuite():
         assert actual_checkout_value_with_illegal_sku_value == -1
 
 
-    def test_checkout_illegal_arguments(self) -> None:
+    def test_checkout_empty_order(self) -> None:
         '''
-        
+        test an empty checkout sku string
         '''
+        #test empty string
+        actual_checkout_value_with_empty_string = checkout_solution.checkout('')
+        assert actual_checkout_value_with_empty_string == 0
+
 
 
 
