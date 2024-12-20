414da50753893f05c69ee35a1e87c0a60bbe7693
sbx_user1051 <sbx_user1051@169.254.7.10>
2024-12-20 14:36:25 +0000

    Fri Dec 20 14:36:25 UTC 2024

--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -1,10 +1,20 @@
 
+from lib.solutions.CHK import checkout_solution
 
 class TestCheckoutSuite():
     '''
-    
+    suite for testing checkout solution
     '''
 
     def test_checkout(self) -> None:
         '''
+        test the checkout method
         '''
+
+        #test normal behaviour without applica
+        test_sku_string: str = 'ABCD'
+        actual_checkout_value = checkout_solution.checkout(test_sku_string)
+        expected_checkout_value = 50 + 30 + 20 + 15
+        assert actual_checkout_value == expected_checkout_value
+
+
