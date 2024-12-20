56b916069fee104af114bdc9952238447a078a49
sbx_user1051 <sbx_user1051@169.254.7.10>
2024-12-20 14:17:38 +0000

    Fri Dec 20 14:17:38 UTC 2024

--- test/solution_tests/HLO/test_hello_world.py
+++ test/solution_tests/HLO/test_hello_world.py
@@ -1,4 +1,18 @@
 
+from lib.solutions.HLO.hello_solution import hello
 
 class TestHelloWorld:
-    
+    '''
+    Hello World Test Suite
+    '''
+    def test_hello() -> None:
+        '''
+        test hello world
+        '''
+        test_name: str = 'Tom'
+        response_string = hello(test_name)
+
+        assert response_string == f'Hello {test_name}!'
+
+
+
