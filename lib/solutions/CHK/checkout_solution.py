5f8511925b36119256f1b04febf05734cc6d7fbe
sbx_user1051 <sbx_user1051@169.254.7.10>
2024-12-20 14:45:25 +0000

    Fri Dec 20 14:45:25 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,19 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+def checkout(skus: str) -> int:
+    '''
+    Our price table and offers: 
+    +------+-------+----------------+
+    | Item | Price | Special offers |
+    +------+-------+----------------+
+    | A    | 50    | 3A for 130     |
+    | B    | 30    | 2B for 45      |
+    | C    | 20    |                |
+    | D    | 15    |                |
+    +------+-------+----------------+
+    '''
+    if len(skus) == 0: return -1
+    if not isinstance()
+
+
--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -10,7 +10,6 @@ class TestCheckoutSuite():
         '''
         test the checkout method
         '''
-
         #test normal behaviour without any applicable special offers
         test_sku_string_no_offer: str = 'ABCD'
         actual_checkout_value_no_offer = checkout_solution.checkout(test_sku_string_no_offer)
@@ -28,10 +27,17 @@ class TestCheckoutSuite():
         actual_checkout_value_with_number_arg = checkout_solution.checkout(test_illegal_number_arg)
         assert actual_checkout_value_with_number_arg == -1
 
-
         #test illegal SKU value
         test_illegal_sku = 'AXVB'
-        
+        actual_checkout_value_with_illegal_sku_value = checkout_solution.checkout(test_illegal_sku)
+        assert actual_checkout_value_with_illegal_sku_value == -1
+
+        #test empty string
+        actual_checkout_value_with_empty_string = checkout_solution.checkout('')
+        assert actual_checkout_value_with_empty_string == -1
+
+
+
 
 
 
